b47a9e822464729895412ccd1b49af81f90dd155
Andrew Boltachev <andrewboltachev@gmail.com>
2013-04-07 20:58:27 +0400

    Добавил файл sses.py

---
+++ sses.py
@@ -0,0 +1,11 @@
+# -*- coding: UTF-8 -*-
+
+import fileinput
+
+def generate_report()
+
+if __name__ == '__main__':
+    text = u' '.join([line for line in fileinput.input()])
+    # ...
+
+    #print report
